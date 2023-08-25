03d95821f73c576e3b459f6f62743238f469adbe
dipendrasingh100 <dipsingh9950@gmail.com>
2023-08-25 20:38:04 +0530

    removed unused import

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import Image from '../components/Image'
 import Contact from './Contact'
 import About from './About'

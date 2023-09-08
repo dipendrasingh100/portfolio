6d83785067e45644b8391f131f8bd6722de9b8d1
dipendrasingh100 <dipsingh9950@gmail.com>
2023-09-08 23:16:54 +0530

    backspeed increased

--- src/components/Image.js
+++ src/components/Image.js
@@ -12,7 +12,7 @@ const Image = () => {
     const typed = new Typed(title.current, {
       strings: ['Dipendra', 'Full-Stack Developer'],
       typeSpeed: 100,
-      backSpeed: 100,
+      backSpeed: 50,
       backDelay: 2000,
       loop: true
     });
